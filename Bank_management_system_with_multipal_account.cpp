dda686edd24833430f07cc550129c9925f26e9c5
Vaghasiya Dhruvin <dhruvinvaghasiya297@gmail.com>
2025-02-04 10:52:12 +0530

    Create Bank_management_system_with_multipal_account.cpp
    Design a simple class BankAccount to represent a bank account. It should have
    the following:
    Attributes:
    o Account holder's name, account number, and balance.
    Methods:
    o deposit(amount): to add money to the account.
    o withdraw(amount): to deduct money from the account if sufficient balance
    exists. Otherwise, print an error message.
    o display_balance() to show the current balance.
    Requirements:
    o Demonstrate object-oriented principles: encapsulation and abstraction.
    o Test the class by creating multiple accounts and performing operations.

---
+++ Bank_management_system_with_multipal_account.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+using namespace std;
+class  bank
+{
+public :
+    string name;
+    double anum;
+    float balance;
+
+    void deposit( )
+    {
+        float bl;
+        cout << "Enter the amount you want to add :";
+        cin >> bl;
+        balance+=bl;
+        cout <<"You'r successfully add amount in your account."<<endl;
+    }
+    void withdraw(float x )
+    {
+        balance -= x;
+        if(balance <0)
+        {
+            cout << "You have not enough amount for withdraw ."<<endl;
+        }
+        else
+        cout << "You'r successfully withdraw your amount ." << endl<<"Current amount is : "<<balance<<endl ;
+
+    }
+    void abalance ()
+    {
+        cout << "Your current balance is : "<<balance <<endl;
+    }
+};
+
+int main()
+{
+    int n;
+
+    double num,a;
+    short int c;
+    cout << "How many account you want to create : ";
+    cin >>n;
+    class bank b[n];
+    for(int i=0; i<n;i++)
+    {
+        cout <<"--------------------------------------------------------"<<endl;
+        cout <<i+1<<") Enter bank account holder name : ";
+        cin >>b[i].name;
+        cout <<i+1<<") Enter bank account number : ";
+        cin >>b[i].anum;
+        cout <<i+1<<") Enter bank account Balance : ";
+        cin >>b[i].balance;
+
+
+    }
+    x:
+        cout <<"--------------------------------------------------------"<<endl;
+        cout <<"Enter choice "<<endl<<"1) deposit "<<endl<<"2) withdraw"<<endl<<"3) check the balance"<<endl<<"4) exit "<<endl<< endl;
+        cout <<"Enter choice: ";
+        cin >>c;
+
+        cout <<"--------------------------------------------------------"<<endl;
+    switch(c)
+    {
+    case 1:
+        cout<<"Enter bank account number :";
+        cin >>num;
+        for(int i=0;i<n;i++)
+        {
+            if(b[i].anum==num)
+            {
+                b[i].deposit( );
+                break;
+            }
+        }
+        goto x;
+        break;
+    case 2:
+        cout<<"Enter bank account number :";
+        cin >>num;
+        for(int i=0;i<n;i++)
+        {
+            if(b[i].anum==num)
+            {
+                cout << "Enter the amount you want to withdraw :";
+                cin >> a;
+                b[i].withdraw(a);
+                break;
+            }
+        }
+        goto x;
+        break;
+    case 3:
+        cout<<"Enter bank account number :";
+        cin >>num;
+        for(int i=0;i<n;i++)
+        {
+            if(b[i].anum==num)
+            {
+                cout << "Your current amount is :"<<b[i].balance<<endl;
+                break;
+            }
+        }
+        goto x;
+        break;
+    case 4:
+        cout <<"Thank you !!";
+        break;
+    default :
+        cout <<"please enter valid choice .";
+        goto x;
+    }
+return 0;
+}
