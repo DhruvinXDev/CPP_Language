c79a4caa5e93e8e7ef708ce16594b05040fea028
Vaghasiya Dhruvin <dhruvinvaghasiya297@gmail.com>
2025-02-24 11:24:10 +0530

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main( )
+{
+    string dhruvin = "vaghasiya dhruvin vipulbhai";
+    cout << dhruvin.length( );
+    cout << dhruvin.capacity();//capacity is slitlly >= length
+}
