59e19f16a059f4c50de1d54ded17d38a048ac49c
DhruvinXDev <dhruvinvaghasiya297@gmail.com>
2025-04-01 01:10:47 +0530

    add file

--- Bank_management_system_with_multipal_account.cpp
+++ Bank_management_system_with_multipal_account.cpp
@@ -1,136 +1,134 @@
 #include<iostream>
+#include<iomanip> // Include for fixed and setprecision
+
 using namespace std;
-class  bank
+
+class bank
 {
-public :
+public:
     string name;
     double anum;
     float balance;
 
-    void deposit( )
+    void deposit()
     {
         float bl;
-        cout << "Enter the amount you want to add :";
+        cout << "Enter the amount you want to add: ";
         cin >> bl;
-        balance+=bl;
-        cout <<"You'r successfully add amount in your account."<<endl;
-        cout << "Your current balance is : "<<balance <<endl;
+        balance += bl;
+        cout << "You successfully added the amount to your account." << endl;
+        cout << "Your current balance is: " << fixed << setprecision(0) << balance << endl;
     }
-    void withdraw(float x )
-    {
 
-        if(balance <0)
+    void withdraw(float x)
+    {
+        if (balance <= 0 || balance < x) // Check if balance is insufficient
         {
-            cout << "You have not enough amount for withdraw ."<<endl;
+            cout << "You do not have enough balance to withdraw." << endl;
         }
         else
         {
             balance -= x;
-            cout << "You'r successfully withdraw your amount ." << endl<<"Current balance is : "<<balance<<endl ;
+            cout << "You successfully withdrew your amount." << endl;
+            cout << "Current balance is: " << fixed << setprecision(0) << balance << endl;
         }
-
     }
-    void abalance ()
+
+    void abalance()
     {
-        cout << "Your current balance is : "<<balance <<endl;
+        cout << "Your current balance is: " << fixed << setprecision(0) << balance << endl;
     }
 };
 
 int main()
 {
-    int n,p=0;
-
-    double num,a;
+    int n;
+    double num, a;
     short int c;
-    cout << "How many account you want to create : ";
-    cin >>n;
-    class bank b[n];
-    for(int i=0; i<n;i++)
+    
+    cout << "How many accounts do you want to create? ";
+    cin >> n;
+    
+    bank b[n]; // Creating an array of bank accounts
+    
+    for (int i = 0; i < n; i++)
     {
-        cout <<"--------------------------------------------------------"<<endl;
-        cout <<i+1<<") Enter bank account holder name : ";
-        cin >>b[i].name;
-        cout <<i+1<<") Enter bank account number : ";
-        cin >>b[i].anum;
-        cout <<i+1<<") Enter bank account Balance : ";
-        cin >>b[i].balance;
-
-
+        cout << "--------------------------------------------------------" << endl;
+        cout << i + 1 << ") Enter bank account holder name: ";
+        cin >> b[i].name;
+        cout << i + 1 << ") Enter bank account number: ";
+        cin >> b[i].anum;
+        cout << i + 1 << ") Enter bank account balance: ";
+        cin >> b[i].balance;
     }
-    x:
-        cout <<"--------------------------------------------------------"<<endl;
-        cout <<"Enter choice "<<endl<<"1) deposit "<<endl<<"2) withdraw"<<endl<<"3) check the balance"<<endl<<"4) exit "<<endl<< endl;
-        cout <<"Enter choice: ";
-        cin >>c;
 
-        cout <<"--------------------------------------------------------"<<endl;
-    switch(c)
+x: 
+    cout << "--------------------------------------------------------" << endl;
+    cout << "Enter choice " << endl
+         << "1) Deposit " << endl
+         << "2) Withdraw " << endl
+         << "3) Check the balance" << endl
+         << "4) Exit " << endl << endl;
+    cout << "Enter choice: ";
+    cin >> c;
+
+    cout << "--------------------------------------------------------" << endl;
+
+    switch (c)
     {
     case 1:
-        cout<<"Enter bank account number :";
-        cin >>num;
-        for(int i=0;i<n;i++)
+        cout << "Enter bank account number: ";
+        cin >> num;
+        for (int i = 0; i < n; i++)
         {
-            p=0;
-            if(b[i].anum==num)
+            if (b[i].anum == num)
             {
-                p++;
-                b[i].deposit( );
-                break;
+                b[i].deposit();
+                goto x;
             }
         }
-        if(p!=1)
-        {
-            cout<<"This account number has not available."<<endl;
-        }
-
-        p=0;
+        cout << "This account number is not available." << endl;
         goto x;
-        break;
+        
     case 2:
-        cout<<"Enter bank account number :";
-        cin >>num;
-        for(int i=0;i<n;i++)
+        cout << "Enter bank account number: ";
+        cin >> num;
+        for (int i = 0; i < n; i++)
         {
-            p=0;
-            if(b[i].anum==num)
+            if (b[i].anum == num)
             {
-                p++;
-                cout << "Enter the amount you want to withdraw :";
+                cout << "Enter the amount you want to withdraw: ";
                 cin >> a;
                 b[i].withdraw(a);
-                break;
+                goto x;
             }
         }
-        if(p!=1)
-            cout<<"This account number has not available."<<endl;
-        p=0;
+        cout << "This account number is not available." << endl;
         goto x;
-        break;
+
     case 3:
-        cout<<"Enter bank account number :";
-        cin >>num;
-        for(int i=0;i<n;i++)
+        cout << "Enter bank account number: ";
+        cin >> num;
+        for (int i = 0; i < n; i++)
         {
-            p=0;
-            if(b[i].anum==num)
+            if (b[i].anum == num)
             {
-                p++;
-                cout << "Your current amount is :"<<b[i].balance<<endl;
-                break;
+                cout << "Your current balance is: " << fixed << setprecision(0) << b[i].balance << endl;
+                goto x;
             }
         }
-        if(p!=1)
-            cout<<"This account number has not available."<<endl;
-        p=0;
+        cout << "This account number is not available." << endl;
         goto x;
-        break;
+
     case 4:
-        cout <<"Thank you !!";
+        cout << "Thank you!!" << endl;
+        cout<<"My name is : Dhruvin Vaghasiya."<<endl<<"My id is : 24CE137."<<endl;
         break;
-    default :
-        cout <<"please enter valid choice .";
+
+    default:
+        cout << "Please enter a valid choice." << endl;
         goto x;
     }
-return 0;
+
+    return 0;
 }
