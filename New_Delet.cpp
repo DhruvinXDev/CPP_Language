391481cdd79f231af7ac6c8a0e317aaf77f6cdd6
dhruvinxdev <dhruvinvaghasiya297@gmail.com>
2025-05-12 00:59:51 +0530

    add file

--- New_Delet.cpp
+++ New_Delet.cpp
@@ -5,15 +5,21 @@ int main()
 {
     int a =4;
     int *ptr = &a; 
-    cout << "The value of the a is the : "<<*ptr<<endl;
+    cout << "The value of the a is : "<<*ptr<<endl;
     // new keyword
     float *f = new float(10.89);
     cout << "The value of the f is : "<<*f  << endl;
+    delete f; // delete the f pointer 
     // new array keyword 
     int *p = new int[40];
     *p = 0;
     *(p +1) = 1;
     cout<<"The value of the p is : "<<*p<<endl;
     cout<<"The value of the p +1 is : "<<*(p +1)<<endl;
+    cout << "The value of the p[0] is : " <<p[0]<<endl;
+    cout<<"The value of the p[1] is : " << p[1]<<endl;
+    //delete the pointer 
+    delete p;
+     
     return 0;
 }
