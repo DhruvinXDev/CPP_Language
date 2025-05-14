157126eca03e157b5dc75f4e05217b8301ca4bd2
dhruvinxdev <dhruvinvaghasiya297@gmail.com>
2025-05-15 02:19:27 +0530

    modifie file

--- Puzzle/extra.cpp
+++ Puzzle/extra.cpp
@@ -3,7 +3,7 @@ using namespace std;
    
 int main() {
    int x =1 , y =5 , z = 5, a = 0;
-   int ans = ++x + !!!!x + z++ + !!!a + y > z ?   x > y ? !!!z : 78 :1  ;
+   int ans = ++x + !!!!x + z++ + !!!a + y > z ?   x < y ? !!!z : 78 :1  ;
    cout << "ans : "<< ans <<endl;
   return 0;
 }
