9223ad66bb051f7d0a3eeca618f21cab5700e89a
dhruvinxdev <dhruvinvaghasiya297@gmail.com>
2025-05-15 02:17:00 +0530

    add file

---
+++ Puzzle/extra.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+   
+int main() {
+   int x =1 , y =5 , z = 5, a = 0;
+   int ans = ++x + !!!!x + z++ + !!!a + y > z ?   x > y ? !!!z : 78 :1  ;
+   cout << "ans : "<< ans <<endl;
+  return 0;
+}
