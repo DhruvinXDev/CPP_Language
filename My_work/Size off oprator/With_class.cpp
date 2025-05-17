b692d276a34bf472ca78ba0b48d32a24a7b1074b
dhruvinxdev <dhruvinvaghasiya297@gmail.com>
2025-05-18 01:45:42 +0530

    add the file in size of folder

---
+++ My_work/Size off oprator/With_class.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+/* Output are :
+
+*/
+class base 
+{
+    public :
+
+};
+
+class drived : public base
+{
+    public :
+};
+int main() {
+   base b;
+   drived d;
+    cout<<"size of the base class : "<<sizeof(b)<<endl;
+    cout<<"size of the drived class : "<< sizeof(d)<< endl;
+  return 0;
+}
+
