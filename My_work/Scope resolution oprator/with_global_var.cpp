28488257a80682da5127b89a99228e35ca48295b
dhruvinxdev <dhruvinvaghasiya297@gmail.com>
2025-05-18 02:37:26 +0530

    add file

--- My_work/Scope resolution oprator/with_global_var.cpp
+++ My_work/Scope resolution oprator/with_global_var.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int x = 120 ;//global variable 
+int x = 120; // global variable
 
-void function ( )
+void function()
 {
-    int x = 10 ;
+    int x = 10;
     {
-        int x =20;
-        cout << "In block x : " << x<<endl;
-        cout << "In block ::x : " <<::x<<endl;
+        int x = 20;
+        cout << "In block x : " << x << endl;
+        cout << "In block ::x : " << ::x << endl;
     }
-    cout << "outside block x : " << x<<endl;
-    cout << "outside block ::x : " <<::x<<endl;
-    
+    cout << "outside block x : " << x << endl;
+    cout << "outside block ::x : " << ::x << endl;
 }
-int main() {
-   function();
-  return 0;
+int main()
+{
+    function();
+    return 0;
 }
