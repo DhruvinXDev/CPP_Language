6a416a3e5eac201d7a08ae016d85781502debca3
dhruvinxdev <dhruvinvaghasiya297@gmail.com>
2025-05-17 23:20:01 +0530

    add extra file

--- Puzzle/extra.cpp
+++ Puzzle/extra.cpp
@@ -5,5 +5,8 @@ int main() {
    int x =1 , y =5 , z = 5, a = 0;
    int ans = ++x + !!!!x + z++ + !!!a + y > z ?   x < y ? !!!z : 78 :1  ;
    cout << "ans : "<< ans <<endl;
+   
   return 0;
+
+
 }
