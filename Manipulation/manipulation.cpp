e58f42d14b4b6cdd25ffcbcd240f4d66ae7090c3
dhruvinxdev <dhruvinvaghasiya297@gmail.com>
2025-05-19 01:53:21 +0530

    update file

--- Manipulation/manipulation.cpp
+++ Manipulation/manipulation.cpp
@@ -10,6 +10,6 @@ int main ()
     cout<<setw(7)<<setfill('0')<<"12399"<<endl; // blank space ne '0' thi fill kerse 
     cout<<right<<setw(7)<<"12399"<<endl; // right side print thase
     cout<<left<<setw(7)<<"12399"<<endl; // right side print thase
-    system("CLS");//clear the screen
+    //system("CLS");//clear the screen
     return 0;
 }
