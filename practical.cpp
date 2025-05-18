f5e8f7085e9a6a7c98389e28b0365eece7c3db4d
dhruvinxdev <dhruvinvaghasiya297@gmail.com>
2025-05-19 02:36:25 +0530

    add file

--- practical.cpp
+++ practical.cpp
@@ -1,60 +1,67 @@
 #include <iostream>
+#include <string>
 using namespace std;
-class faculty ;
 
+class student;
 
-class student
-{
+class faculty {
+    int facultyid;
+    string facultyname;
+public:
+    void add_faculty_personal_data() {
+        cout << "Enter faculty ID and name: ";
+        cin >> facultyid;
+        cin.ignore(); // to ignore leftover newline
+        getline(cin, facultyname);
+    }
+
+    void add_math_marks(student& x);
+};
+
+class student {
     int studentid, mathmark, cmark;
     float per;
-    char studentname[25];
+    string studentname;
 public:
-    void add_student_personal_data()
-    {
-        cin >> studentid >> studentname;
+    void add_student_personal_data() {
+        cout << "Enter student ID and name: ";
+        cin >> studentid;
+        cin.ignore(); // to ignore leftover newline
+        getline(cin, studentname);
     }
-    
 
-    friend void faculty :: add_math_marks(student &x);
+    friend void faculty::add_math_marks(student& x);
+
+    void display_student_data() {
+        cout << "ID: " << studentid << ", Name: " << studentname
+             << ", Math Marks: " << mathmark << endl;
+    }
 };
 
-class faculty
-{
-    int facultyid;
-    char facultyname[25];
-public:
-    void add_faculty_personal_data()
-    {
-        cin >> facultyid >> facultyname;
-    }
+void faculty::add_math_marks(student& x) {
+    cout << "Enter math marks for student ID " << x.studentid << ": ";
+    cin >> x.mathmark;
+}
 
-    void add_math_marks(student &x );
-
-};
-
-void faculty :: add_math_marks(student &x)
-    {
-        cin >> x.mathmark;
-    }
-
-int main()
-{
+int main() {
     student s[5];
-    faculty f1 , f2;
-    int i;
+    faculty f1, f2;
 
-    for (i=0;i<5;i++)
-    {
+    for (int i = 0; i < 5; i++) {
         s[i].add_student_personal_data();
     }
 
     f1.add_faculty_personal_data();
     f2.add_faculty_personal_data();
 
-    for(int i=0;i<5;i++)
-    {
+    for (int i = 0; i < 5; i++) {
         f1.add_math_marks(s[i]);
     }
-    
+
+    cout << "\nStudent Data:\n";
+    for (int i = 0; i < 5; i++) {
+        s[i].display_student_data();
+    }
+
     return 0;
 }
