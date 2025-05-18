22a3178dc9bc42b138db3adfb7b68b645afc7d84
dhruvinxdev <dhruvinvaghasiya297@gmail.com>
2025-05-18 16:11:58 +0530

    modified file are added

--- My_work/virtual/size_variation.cpp
+++ My_work/virtual/size_variation.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
-
+/*
+The size of the A class : 1
+The size of the B class : 4
+The size of the C class : 1
+The size of the D class : 4
+*/
 class A {};
 
 class B {
@@ -14,11 +19,17 @@ class C {
 class D {
     virtual void foo();
 };
+class E {
+    virtual void foo();
+};
    
 int main() {
    cout<<"The size of the A class : "<<sizeof(A)<<endl;
    cout<<"The size of the B class : "<<sizeof(B)<<endl;
    cout<<"The size of the C class : "<<sizeof(C)<<endl;
    cout<<"The size of the D class : "<<sizeof(D)<<endl;
+   cout<<"The size of the E class : "<<sizeof(E)<<endl;
+   cout<<"The size of virtual : "<<sizeof(virtual)<<endl;
+
   return 0;
 }
