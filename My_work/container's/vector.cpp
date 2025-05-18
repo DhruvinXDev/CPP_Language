4c3820d4286cc4b86ffb206115b39302cb9bfd6e
dhruvinxdev <dhruvinvaghasiya297@gmail.com>
2025-05-18 16:12:52 +0530

    add vector file in the folder

---
+++ My_work/container's/vector.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// void print(vector<int> &vect) const // give error because this is not valid syntex in normal fun it valid in class
+// {
+//     for(auto i : vect)
+//     {
+//         cout<< i << " ";
+//     }
+// }
+
+void print(const vector<int> &vect)
+{
+    for (auto i : vect)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> v(2);
+    print(v);
+    vector<int> v1(5, 10);
+    print(v1);
+    v1.push_back(1);
+    print(v1);
+    // v1.push_front(1); //give error because this in not valid in vector
+
+    return 0;
+}
