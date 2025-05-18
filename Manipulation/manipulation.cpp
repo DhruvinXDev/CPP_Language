c7aacfaa1a74c5bdd94e4e323bdbac7d3d5f9ebb
dhruvinxdev <dhruvinvaghasiya297@gmail.com>
2025-05-19 02:00:23 +0530

    add file

--- Manipulation/manipulation.cpp
+++ Manipulation/manipulation.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 #include<iomanip>
 using  namespace  std;
+/*
+375.722
+3.8e+002
+375.72
+11
+0012399
+0012399
+1239900
+*/
 int main ()
 {
     cout<<375.7216<<endl; //coman output
@@ -10,6 +19,6 @@ int main ()
     cout<<setw(7)<<setfill('0')<<"12399"<<endl; // blank space ne '0' thi fill kerse 
     cout<<right<<setw(7)<<"12399"<<endl; // right side print thase
     cout<<left<<setw(7)<<"12399"<<endl; // right side print thase
-    //system("CLS");//clear the screen
+    system("CLS");//clear the screen
     return 0;
 }
